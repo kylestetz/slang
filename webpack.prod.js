cd1e1fce8c15593f6fd0ca648f7c55aba4f36efa
Kyle Stetz <kstetz@slack-corp.com>
2018-09-30 13:22:51 -0700

    Use babel in prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,6 +11,16 @@ module.exports = {
     rules: [{
       test: /\.css$/,
       use: [ 'style-loader', 'css-loader' ]
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
     }],
   },
 };
